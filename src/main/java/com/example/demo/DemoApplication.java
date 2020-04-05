eb5204002907679a3693fe7c3ba473993b733738
srq <504105925@qq.com>
2020-04-05 20:54:36 +0800

    完整

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,13 +7,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-
+@EnableScheduling // 开启定时
 @MapperScan("com.example.demo.dao.mapper")
 public class DemoApplication extends  WebMvcConfigurerAdapter{
 
--- src/main/java/com/example/demo/contronller/AdminUserController.java
+++ src/main/java/com/example/demo/contronller/AdminUserController.java
@@ -1,13 +1,19 @@
 package com.example.demo.contronller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.example.demo.dao.mapper.UserMapper;
+import com.example.demo.dao.pojo.User;
 import com.example.demo.jwt.JwtIgnore;
 import com.example.demo.jwt.Audience;
 import com.example.demo.jwt.JwtTokenUtil;
+import com.example.demo.timedtask.ScheduledData;
 import com.example.demo.uitl.BaseResponse;
 import com.example.demo.uitl.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,31 +30,41 @@ import java.util.UUID;
 @Slf4j
 @RestController
 public class AdminUserController {
-
+    private static final Logger loger = LoggerFactory.getLogger(AdminUserController.class);
     @Autowired
     private Audience audience;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @PostMapping("/login")
     @JwtIgnore
-    public BaseResponse<JSONObject>  adminLogin(HttpServletResponse response, String username,String password) {
+    public BaseResponse<JSONObject>  adminLogin(HttpServletResponse response, User userInfo) {
         BaseResponse<JSONObject>  rs  =  new BaseResponse<>();
+        JSONObject result = new JSONObject();
         // 这里模拟测试, 默认登录成功，返回用户ID和角色信息
-        String userId = UUID.randomUUID().toString();
-        String role = "admin";
+        User loginUser =  userMapper.selectAnyParams(userInfo);
+        if (null == loginUser || null == userInfo.getUseremail() || null == userInfo.getPwd() ){
+            rs.setStatusCode(201);
+            result.put("statusMsg","请输入正确的邮箱或密码！");
+            rs.setResponseData(result);
+            return rs;
+        }
         // 创建token
-        String token = JwtTokenUtil.createJWT(userId, username, role, audience);
+        String token = JwtTokenUtil.createJWT(loginUser.getUserid(), loginUser.getUsername(), "user", audience);
         log.info("### 登录成功, token={} ###", token);
         // 将token放在响应头
         response.setHeader(JwtTokenUtil.AUTH_HEADER_KEY, JwtTokenUtil.TOKEN_PREFIX + token);
         // 将token响应给客户端
-        JSONObject result = new JSONObject();
         result.put("token", token);
+        rs.setStatusCode(200);
         rs.setResponseData(result);
         return  rs;
     }
     @GetMapping("/users")
     public Result userList() {
         log.info("### 查询所有用户列表 ###");
+        loger.info("### 查询所有用户列表 ###");
         return Result.SUCCESS();
     }
 }
